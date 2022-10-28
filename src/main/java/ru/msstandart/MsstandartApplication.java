ec80fcab664a2a1e4d4423c30b029ee3eacf17ae
bafkit <konyrevpv@mail.ru>
2022-10-28 21:09:49 +0300

    Initial commit

---
+++ src/main/java/ru/msstandart/MsstandartApplication.java
@@ -0,0 +1,13 @@
+package ru.msstandart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsstandartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsstandartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/msstandart/MsstandartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.msstandart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsstandartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
